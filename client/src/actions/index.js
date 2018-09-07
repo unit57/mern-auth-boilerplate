c22c0216678175d000f61fff35987b839b11a348
Eric Sanchez <eric.sanchez@domandtom.com>
2018-09-07 10:37:38 -0400

    course complete '

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -14,10 +14,22 @@ export const signup = ( formProps, callback )=> async dispatch => {
     dispatch({ type: AUTH_ERROR, payload: 'Email in use' })
   }
 };
+export const signin = ( formProps, callback )=> async dispatch => {
+  try {
+    const response = await axios.post('http://localhost:3090/signin', formProps )
+    dispatch({
+      type: AUTH_USER,
+      payload: response.data.token
+    });
+    localStorage.setItem('token', response.data.token);
+    callback();
+  } catch (e) {
+    dispatch({ type: AUTH_ERROR, payload: 'Invalid Login Credentials' })
+  }
+};
 
 export const signout = () => {
   localStorage.removeItem('token');
-
   return {
     type: AUTH_USER,
     payload: ''
